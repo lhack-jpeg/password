78afbab1ac1ca623b073e292fdb8cd1657a041f0
lhack-jpeg <luke-hackett@outlook.com>
2022-03-30 19:59:45 +1100

    created basic serach function using knex

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ const path = require('path');
 const ejsMate = require('ejs-mate');
 const mysql = require('mysql');
 const mysql2 = require('mysql2');
+const { response } = require('express');
+const knex = require('knex')({
+    client: 'mysql2',
+    connection: {
+        host: 'localhost',
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: 'password_manager',
+    },
+});
 
 const app = express();
 app.engine('ejs', ejsMate);
@@ -15,23 +25,30 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: 'password_manager',
-});
+// connection.connect((err) => {
+//     if (err) {
+//         console.error('error connecting ' + err.stack);
+//     }
 
-connection.connect((err) => {
-    if (err) {
-        console.error('error connecting ' + err.stack);
-    }
+//     console.log(`Connected via ${connection.threadId}`);
+// });
 
-    console.log(`Connected via ${connection.threadId}`);
-});
-
-app.get('/', (req, res) => {
-    res.send('Sever is active');
+app.get('/', async (req, res) => {
+    const showRows = knex
+        .from('passwords')
+        .select('*')
+        .then((rows) => {
+            for (row of rows) {
+                res.send(row);
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+            throw err;
+        })
+        .finally(() => {
+            knex.destroy();
+        });
 });
 
 const port = 3000 || process.env.PORT;
